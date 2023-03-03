9c604b1535d60de6fbb529f707151bf85f5c4afe
Zafk1eI <sintfrank@gmail.com>
2023-03-03 20:07:21 +0300

    Добавьте файлы проекта.

---
+++ inheritance/Personal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    internal class Personal
+    {
+        private string firstname;
+        private string lastname;
+        private int Age;
+        private string position; // job title
+        public string FirstName
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+        public int age
+        {
+            get { return Age; }
+            set { Age = value; }
+        }
+        public string Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+        public void Write()
+        {
+            Console.WriteLine(FirstName + " " + LastName + '\n' + "Age: " + Age + '\n' + "Занимает должность: " + Position);
+        }
+    }
+    internal class Bomsh : Personal // наслдование
+    {
+        private string residence;
+        public string Residence
+        {
+            get { return residence; }
+            set { residence = value; }
+        }
+            public void writeResidence()
+        {
+            Console.WriteLine("местожительство: " + residence);
+        }
+    }
+}
---
+++ inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personal personal = new Personal() { LastName = "Pupkin", FirstName = "Vasya", age = 14, Position = "junior" };
+            Bomsh bomsh = new Bomsh() { LastName = "Безпечени", FirstName = "Петя", age = 65, Residence = "Улица" };
+            Console.WriteLine("Человек без определенного места жительства:");
+            bomsh.Write();
+            bomsh.writeResidence();
+            Console.WriteLine("\nРаботник комании: ");
+            personal.Write();
+            Console.ReadKey();
+        }
+    }
+}
