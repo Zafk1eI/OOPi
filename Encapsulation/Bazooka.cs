7fc698c9c22e02dfdfb610f9950f94d478857f79
Zafk1eI <sintfrank@gmail.com>
2023-03-03 20:02:12 +0300

    update

---
+++ Encapsulation/Bazooka.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Bazooka
+    {
+        private bool isLoaded = false;
+        private void Reload() 
+        {
+            isLoaded = true;
+            Console.WriteLine("Базука заряжена\n");
+        }
+        public void Shoot()
+        {
+            if (isLoaded == false)
+            {
+                Console.WriteLine("Базука не заряжена\n");
+                Reload();
+            }
+            Console.WriteLine("Boom!!!\n");
+            isLoaded = false;
+        }
+    }
+}
