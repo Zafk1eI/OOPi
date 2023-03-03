151b4dbc414dfced38a7d218b7ebf198d897c598
Zafk1eI <sintfrank@gmail.com>
2023-03-03 21:45:08 +0300

    Добавьте файлы проекта.

---
+++ Basis/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[10];
+            Random rand = new Random();
+            Sort bubble = new Sort();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = rand.Next(20);
+                Console.WriteLine("Значение элемента массива " + i + " = " + mas[i]);
+            }
+            bubble.BubbleSort(mas);
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine("отсортированные Значения элемента массива " + i + " = " + mas[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Basis/Sort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basis
+{
+    internal class Sort
+    {
+        public int[] BubbleSort(int[] mas)
+        {
+            int swapper = 0;
+            for (var i = 1; i < mas.Length; i++)
+            {
+                for (var j = 0; j < mas.Length - i; j++)
+                {
+                    if (mas[j] > mas[j + 1])
+                    {
+                        swapper = mas[j];
+                        mas[j] = mas[j + 1];
+                        mas[j + 1] = swapper;
+                    }
+                }
+            }
+            return mas;
+        }
+    }
+}
