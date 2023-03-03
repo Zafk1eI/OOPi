1b1470197e47ee7a1146f64c651a767997a2a518
Zafk1eI <sintfrank@gmail.com>
2023-03-03 20:47:06 +0300

    Добавьте файлы проекта.

---
+++ incap/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace incap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bazooka bazooka = new Bazooka();
+            bazooka.Shoot();
+            Console.ReadKey();
+        }
+    }
+}
